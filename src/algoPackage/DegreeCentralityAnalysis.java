5f534cf9406334cfc174073d04e8dab8d3ed0bfa
pagai <pagai@phobos.fritz.box>
2021-07-28 06:38:30 +0200

    added better output and javadoc

--- src/algoPackage/DegreeCentralityAnalysis.java
+++ src/algoPackage/DegreeCentralityAnalysis.java
@@ -19,6 +19,11 @@ public class DegreeCentralityAnalysis {
 		graphDB = inputGraphDB;
 	}
 
+	/**
+	 * Method to get an output for calculation of all the most central nodes sorted by their degree 
+	 * @param graphDB - the graphDB instance
+	 * @param verbose - if true, the list will be printed out, if false only the runtime will be printed out
+	 */
 	public void getDegreeCentrality(GraphDatabaseService graphDB, boolean verbose) {
 		algoStartTime = System.currentTimeMillis();
 		try (Transaction tx = graphDB.beginTx()) {
@@ -28,14 +33,13 @@ public class DegreeCentralityAnalysis {
 		}
 		Map<String, Integer> sortedDegreeList = degreeList.entrySet().stream().sorted(Map.Entry.comparingByValue())
 				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+		int nodeCounter = sortedDegreeList.size(); 
 		algoEndTime = System.currentTimeMillis();
-		System.out.println("GOT SORTED DEGREES FOR " + sortedDegreeList.size() + " NODES IN " + (algoEndTime - algoStartTime) + " ms.");
+		System.out.println("GOT SORTED DEGREES FOR " + nodeCounter + " NODES IN " + (algoEndTime - algoStartTime) + " ms.");
 		if (verbose) {
 			for (Map.Entry<String, Integer> entryOfList : sortedDegreeList.entrySet()) {
 				System.out.println(entryOfList.getKey() + "\t - " + entryOfList.getValue());
 			}
 		}
-
 	}
-
 }
